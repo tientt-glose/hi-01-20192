b7c731643c7a397f10a968e2687fdedb5fa1ddde
piapip <tho.ph153619@sis.hust.edu.vn>
2020-05-11 19:47:00 +0700

    Add gantt chart to the gantt chart page. Put the add task and the delete button side by side of the chart.

--- src/container/GanttChartPage.js
+++ src/container/GanttChartPage.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
 
+import GanttCRUD from '../component/ForGantt/Gantt_CRUD'
 import Header from '../component/Header'
 
 class GanttChartPage extends Component {
+
+  constructor(props) {
+    super(props);
+    let d1 = new Date();
+    let d2 = new Date();
+    d2.setDate(d2.getDate() + 5);
+    let d3 = new Date();
+    d3.setDate(d3.getDate() + 8);
+    let d4 = new Date();
+    d4.setDate(d4.getDate() + 20);
+
+    let data = [
+      {
+        id: 1,
+        start: d1,
+        end: d2,
+        name: 'Demo Task 1'
+      },
+      {
+        id: 2,
+        start: d3,
+        end: d4,
+        name: 'Demo Task 2',
+        color: 'orange'
+      }
+    ];
+
+    this.state = { data: data }
+  }
+
   render() {
     return (
       <div>
         <Header />
-        This is a Gantt Page.   
+        <GanttCRUD
+          data = {this.state.data} /> 
       </div>
     );
   }
