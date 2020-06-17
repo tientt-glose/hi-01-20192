c0e357eea9c6e69bf8ca35954c2b7cbc53ab392c
tiennguyen <tiennguyenbka198@gmail.com>
2020-06-17 11:28:31 +0700

    A little bit commit

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export default {
   NHOM3: 'https://nguyenvd27.github.io/HI_template/admin',
   NHOM4: 'https://drnguyen2525.github.io/hci-flight-management',
   NHOM5: 'https://electric-report-a846b.web.app',
-  NHOM6: 'http://100.26.57.128/#',
+  NHOM6: 'https://risk-report-6c67e.web.app/#',
   NHOM7: 'https://nnhhai.github.io',
   NHOM8: 'https://sleepy-lowlands-11196.herokuapp.com/drone-management',
   NHOM9: 'https://www.figma.com/proto/vtNY9oE4CgxBhNfIQ83mGY/20192?node-id=1%3A2&scaling=scale-down',
